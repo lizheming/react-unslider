b8fbbdcd1eb18e5affbdedbe567bc87ee0786d57
lizheming <i@imnerd.org>
2020-01-22 16:58:16 +0800

    增加简单demo

--- examples/main.jsx
+++ examples/main.jsx
@@ -5,14 +5,17 @@ import Unslider from '../src';
 function App() {
   return (
     <Unslider 
-      width={400} 
+      width={300} 
       height={189} 
       autoplay={true} 
       delay={3000}
       loop={true}
       speed={700}
+      nav={false}
+      arrow={true}
+      animation="horizontal"
     >
-      {sliders.map((slider, index) => (
+      {new Array(10).fill(0).map((_, index) => (
         <Unslider.Item key={index}>
           Slider {index+1}
         </Unslider.Item>
